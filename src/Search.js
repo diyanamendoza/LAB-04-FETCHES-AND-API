79f7baff63b52c5f4b142bd4aaec5cfcf07dbbda
diyanamendoza <diyana.amp@pm.me>
2021-09-24 11:55:27 -0700

    Add third dropdown for search by category

--- src/Search.js
+++ src/Search.js
@@ -4,8 +4,17 @@ export default class Search extends Component {
     render() {
         return (
             <div className='form-container'>
-                <form onSubmit={this.props.submit}>
-                    <input onChange={this.props.input} placeholder='Enter Pokemon Name'></input>
+                <form onSubmit={this.props.submit} id="searchform">
+                    <input onChange={this.props.input} placeholder='Enter query, then select search type '></input>
+                    <p>Search Type</p>
+                        {
+                        <select onChange={this.props.categorySearch}>
+                            <option value='pokemon'>Name</option>
+                            <option value='type'>Type</option>
+                            <option value='shape'>Shape</option>
+                            <option value='ability'>Ability</option>
+                        </select>
+                        }
                     <button>Search</button>
                 </form>
                 <button onClick={this.props.reset}>Reset</button>
