9ba9d594004de75be12173c291d5d1846b6f3f71
diyanamendoza <diyana.amp@pm.me>
2021-09-24 09:41:39 -0700

    Add react router, home

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import './App.css';
+import React, { Component } from 'react'
+import {
+    BrowserRouter as Router, 
+    Route, 
+    Switch,
+} from 'react-router-dom';
 import SearchPage from './SearchPage.js';
+// import DetailPage from './DetailPage.js';
+import Home from './Home.js';
 
-function App() {
-  return (
-    <div className="App">
-    <SearchPage />
-    </div>
-  );
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <Router>
+                    <Switch>
+                        <Route 
+                            path="/" 
+                            exact
+                            render={(routerProps) => <Home {...routerProps} />} 
+                        />
+                        <Route 
+                            path="/pokemon" 
+                            exact
+                            render={(routerProps) => <SearchPage {...routerProps} />} 
+                        />
+                    </Switch>
+                </Router>
+            </div>
+        )
+    }
 }
-
-export default App;
